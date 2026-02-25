25ca7bbdc50bcd49dbbf3ea9798f7c37ff8d7bdd
mmaus <mark.maus@berkeley.edu>
2026-02-24 16:55:13 -0800

    update get_LSS_data.sh

--- get_LSS_data.sh
+++ get_LSS_data.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+#
 module load python
 conda activate cobaya
-
-mkdir ./data
-mkdir ./data/DR1
-cp -r /global/cfs/cdirs/desi/users/mmaus/C3PO/data ./data/DR1/
+#
+if [ ! -d data ]; then
+    echo "data not found.  Creating it."
+    mkdir ./data
+fi
+if [ ! -d data/DR1 ]; then
+    echo "data/DR1 not found.  Creating it."
+    mkdir ./data/DR1
+fi
+cp -r /global/cfs/cdirs/desi/users/mmaus/C3PO/data/* ./data/DR1/
